19863ad4f48d86621119664c7e64966de4e8908a
Rob Sutton <robbertsutton@gmail.com>
2022-12-25 00:47:45 -0500

    fix: dont show harmless errors while configuring

--- salt/configure.sh
+++ salt/configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo salt-call state.apply pygit # for gitfs
+sudo salt-call state.apply pygit -l quiet # for gitfs
 sudo salt-call state.sls_id chezmoi_init chezmoi # to show chezmoi diff
 sudo chmod -R 777 var/ 2> /dev/null || true
